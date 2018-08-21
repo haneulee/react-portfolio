abbddaa2674d087abed5a4f30313d3bcb2a1e2d3
Haneul Lee <lovesky4294@gmail.com>
2018-08-21 18:51:13 +0900

    add slide component

---
+++ src/components/common/Slide.js
@@ -0,0 +1,151 @@
+import React from "react";
+import styled from "styled-components";
+
+const SlideWrapper = styled.div`
+  height: ${props => props.height - 70}px;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+`;
+
+const UlWrapper = styled.ul`
+  width: 4215%;
+  position: relative;
+  transition-duration: 0.5s;
+  transform: translate3d(
+    ${props => -1 * (props.width * props.translateValue)}px,
+    0px,
+    0px
+  );
+`;
+
+const ListWrapper = styled.li`
+  float: left;
+  list-style: none;
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const ButtonWrapper = styled.div`
+  padding: 0;
+  position: absolute;
+  width: 30px;
+  top: 43.5%;
+  left: 87.5%;
+  height: 135px;
+`;
+
+const ButtonWrap = styled.div`
+  display: block;
+  margin: 15px auto;
+  width: 10px;
+  & [on] {
+    background: #f00;
+  }
+`;
+
+const ButtonLink = styled.a`
+  background: #fff;
+  display: block;
+  width: 10px;
+  height: 10px;
+  outline: 0;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    background: #f00;
+  }
+`;
+
+const List = props => {
+  return (
+    <ListWrapper>
+      <img src={props.url} alt="" style={{ width: props.width }} />
+    </ListWrapper>
+  );
+};
+
+const Button = props => {
+  return (
+    <ButtonWrap>
+      <ButtonLink
+        onClick={e => {
+          let target = e.target.parentNode,
+            parent = target ? target.parentNode : null,
+            list = parent
+              ? Array.prototype.slice.call(parent.childNodes, 0)
+              : [],
+            num = list.indexOf(target);
+
+          list.map((data, index) => {
+            if (index === num) {
+              data.setAttribute("on", "true");
+            } else {
+              data.setAttribute("on", "");
+            }
+          });
+
+          props.context.setState({ translateValue: num, buttonList: list });
+        }}
+      />
+    </ButtonWrap>
+  );
+};
+
+export default class Slide extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: window.innerHeight,
+      width: window.innerWidth,
+      translateValue: 0,
+      buttonList: []
+    };
+  }
+
+  handleResize = () => {
+    let list = this.state.buttonList ? this.state.buttonList : [],
+      target = list ? list[this.state.translateValue] : null,
+      num = this.state.translateValue;
+
+    this.setState({
+      height: window.innerHeight,
+      width: window.innerWidth,
+      translateValue: num
+    });
+  };
+
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  render() {
+    return (
+      <div>
+        <SlideWrapper height={this.state.height}>
+          <UlWrapper
+            translateValue={this.state.translateValue}
+            width={this.state.width}
+          >
+            {this.props.list.map((data, index, arr) => (
+              <List key={index} url={data} width={this.state.width} />
+            ))}
+          </UlWrapper>
+        </SlideWrapper>
+        <ButtonWrapper>
+          {this.props.list.map((data, index, arr) => (
+            <Button key={index} context={this} />
+          ))}
+        </ButtonWrapper>
+      </div>
+    );
+  }
+}
--- src/components/works/WithUs.js
+++ src/components/works/WithUs.js
@@ -1,17 +1,25 @@
 import React from "react";
+import Slide from "../common/Slide";
+import styled, { keyframes } from "styled-components";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+
+const Container = styled.div`
+  width: 100%;
+  flex: 1;
+`;
+
+const imgUrl = [
+  require("../../image/works/withUs/1.jpg"),
+  require("../../image/works/withUs/2.jpg"),
+  require("../../image/works/withUs/3.jpg"),
+  require("../../image/works/withUs/4.jpg")
+];
 
 const WithUs = () => {
   return (
-    <ReactCSSTransitionGroup
-      transitionName="aboutTransition"
-      transitionAppear={true}
-      transitionAppearTimeout={500}
-      transitionEnter={false}
-      transitionLeave={false}
-    >
-      <div>"WithUs"</div>
-    </ReactCSSTransitionGroup>
+    <Container>
+      <Slide list={imgUrl} />
+    </Container>
   );
 };
 
